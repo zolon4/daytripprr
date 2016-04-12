f341797aa27251d26a731a938a79f7b38253b6fb
zolon wilkins <zolonsk8@gmail.com>
2016-04-12 10:52:44 -0500

    about design sweep

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,11 @@
 $(function(){
+
+
+ var map = function(image) {
+  setTimeout(function(){
+   $('#map')
+  })
+ }
  $("#logo2").typed({
         strings: ["lets take a trip ^1000", "daytripprr"],
         typeSpeed: 40
