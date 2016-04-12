f6da1bf717899b30aba54d32c0dcd25f1be5f69b
zolon wilkins <zolonsk8@gmail.com>
2016-04-12 12:18:37 -0500

    about page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,26 @@
 $(function(){
+var map = '<img id="theImg" src="http://www.aperfectworld.org/clipart/seasons/sun.png" />'
 
-
- var map = function(image) {
+ var delayImage = function(image) {
   setTimeout(function(){
-   $('#map')
-  })
+   $('#map').append(map)
+  },4800)
  }
+ $('#bottom').click(function () {
+          $('html, body').animate({
+              scrollTop: $(document).height()
+          },
+          1500);
+         return false;
+      });
+
  $("#logo2").typed({
         strings: ["lets take a trip ^1000", "daytripprr"],
-        typeSpeed: 40
+        typeSpeed: 40,
+        showCursor: false
       });
+  delayImage(map)
+
+
+
 })
