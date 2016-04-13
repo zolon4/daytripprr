7672a65c4c857cf881f3a7d2056d6ae089ad4076
zolon wilkins <zolonsk8@gmail.com>
2016-04-13 15:25:44 -0500

    fixed map on search page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -14,13 +14,12 @@ var map = '<img id="theImg" src="http://images.clipartpanda.com/sun-transparent-
       var saveTrip = '<form id="ABC" action="/search" method="post"><div class="form-group"><input type="hidden" name="destination" value="'+ response.destination +'"><input type="hidden" name="distance" value="'+response.distance+'"><input type="hidden"  name="duration" value="'+response.duration+'"><input type="hidden" class="form-control form-control-custom" name="origin" value="'+response.origin+'"></div><button id="saveTrip" type="submit" class="btn btn-warning btn-block">Save</button></form> '
       console.log(saveTrip);
 
-      var map = 'width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyC-EwkbLF1DAAx4XVC7935uSXUEtiM68iM&origin=' + response.origin + '&destination=' + response.destination + '"';
+      var map = 'frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyC-EwkbLF1DAAx4XVC7935uSXUEtiM68iM&origin=' + response.origin + '&destination=' + response.destination + '"';
       console.log(map);
 
-       $('#result').prepend('<div class="card"><div class="card-block"><div class="row"><div class="col-sm-3"><img src="http://image005.flaticon.com/28/svg/33/33409.svg" class="img-fluid"/></div><div class="col-sm-9"><a target="_blank" href="https://www.google.com/maps/dir/'+response.origin+'/'+response.destination+'/"><h4>' + response.destination + '</h4><p>' + response.duration + '</p><br><iframe ' + map + '></iframe>' + saveTrip +'</a></div></div></div></div>');
+       $('#result').prepend('<div class="card"><div class="card-block"><div class="row"><div class="col-sm-4"><iframe ' + map + '></iframe></div><div class="col-sm-8"><a target="_blank" href="https://www.google.com/maps/dir/'+response.origin+'/'+response.destination+'/"><h4>' + response.destination + '</h4></a><p>' + response.duration + '</p><br>' + saveTrip +'</a></div></div></div></div>');
        debugger;
-       // $('#result').prepend('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC-EwkbLF1DAAx4XVC7935uSXUEtiM68iM&q=Space+Needle,Seattle+WA"> </iframe>');
-
+        // $('#result').prepend('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyC-EwkbLF1DAAx4XVC7935uSXUEtiM68iM&q=Space+Needle,Seattle+WA"> </iframe>');
 
         $('#saveTrip').on('click', function(){
          { saveTrip: $('#ABC').val(); }
