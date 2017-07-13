3daaddf0645cfae4c185a0e3dc1946b9ba360682
Zolon Wilkins <zolonsk8@gmail.com>
2017-07-13 09:40:00 -0500

    fix syntax and ignore node modules

--- config/passport.js
+++ config/passport.js
@@ -1,117 +1,67 @@
-// config/passport.js
-
-// load all the things we need
 var LocalStrategy = require('passport-local').Strategy;
-
-// load up the user model
 var User = require('../models/user');
 
-// expose this function to our app using module.exports
 module.exports = function(passport) {
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
 
-    // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
     });
-
-    // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            done(err, user);
-        });
-    });
-
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
-    passport.use('local-signup', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done) {
-
-        // asynchronous
-        // User.findOne wont fire unless data is sent back
-        process.nextTick(function() {
-
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
-            // if there are any errors, return the error
-            if (err)
-                return done(err);
-
-            // check to see if theres already a user with that email
-            if (user) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-            } else {
-
-                // if there is no user with that email
-                // create the user
-                var newUser = new User();
-                newUser.local.email = email;
-                newUser.local.password = password;
-                newUser.local.username = req.body.username;
-                newUser.local.currentcity = req.body.currentcity;
-                newUser.local.currentstate = req.body.currentstate;
-
-                // set the user's local credentials
-                newUser.local.email = email;
-                newUser.local.password = newUser.generateHash(password);
-
-                // save the user
-                newUser.save(function(err) {
-                    if (err)
-                        throw err;
-                    return done(null, newUser);
-                });
-            }
-
-        });
-
-        });
-
-    }));
+  });
 
 
-   passport.use('local-login', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
+  passport.use('local-signup', new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
   }, function(req, email, password, done) {
+    process.nextTick(function() {
+      User.findOne({ 'local.email' :  email }, function(err, user) {
+        if (err)
+        return done(err);
 
-    // Search for a use with this email
-    User.findOne({ 'local.email': email }, function(err, user) {
-      if (err) return done(err);
+        if (user) {
+          return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+        } else {
 
-      // If no user is found
-      if (!user) return done(null, false, req.flash('errorMessage', 'No user found.'));
+          var newUser = new User();
+          newUser.local.email = email;
+          newUser.local.password = password;
+          newUser.local.username = req.body.username;
+          newUser.local.currentcity = req.body.currentcity;
+          newUser.local.currentstate = req.body.currentstate;
 
-      // Check if the password is correct
-      if (!user.validPassword(password)) return done(null, false, req.flash('errorMessage', 'Oops wrong password!'));
+          // set the user's local credentials
+          newUser.local.email = email;
+          newUser.local.password = newUser.generateHash(password);
 
-      return done(null, user);
-    });
-
+          // save the user
+          newUser.save(function(err) {
+            if (err)
+            throw err;
+            return done(null, newUser);
+          });
+        }
+        });
+      });
   }));
 
 
+   passport.use('local-login', new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+  }, function(req, email, password, done) {
 
-
-
-
-
-
-
+    User.findOne({ 'local.email': email }, function(err, user) {
+      if (err) return done(err);
+      if (!user.validPassword(password)) return done(null, false, req.flash('errorMessage', 'Oops wrong password!'));
+      return done(null, user);
+    });
+  }));
 
 };
